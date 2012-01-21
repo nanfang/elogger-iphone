903e476e9addb257b336b7a3d886beaf496696db
Fang Nan <nanfang05@gmail.com>
2012-01-21 22:48:04 +0800

    init the xcode project

---
+++ src/ELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ELAppDelegate.h
+//  eLogger-iphone
+//
+//  Created by Nan Fang on 1/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ src/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eLogger-iphone
+//
+//  Created by Nan Fang on 1/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ELAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ELAppDelegate class]));
+    }
+}
